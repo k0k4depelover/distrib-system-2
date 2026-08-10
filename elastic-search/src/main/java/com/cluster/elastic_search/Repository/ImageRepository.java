7686c1e90c808632226d69eed3065c06da168eec
kok4depelover <oskarcermeno1@gmail.com>
2026-08-09 19:11:19 -0600

    Agregando filtros de seguridad

---
+++ elastic-search/src/main/java/com/cluster/elastic_search/Repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.cluster.elastic_search.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.cluster.elastic_search.Model.ImagenMD;
+
+public class ImageRepository extends CrudRepository<ImagenMD, Long> {
+    
+}
