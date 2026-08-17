4fff189c8ca1e79e771ecd04d559a945da5c1e9f
kok4depelover <oskarcermeno1@gmail.com>
2026-08-16 19:32:58 -0600

    Creando Thread pool

--- elastic-search/src/main/java/com/cluster/elastic_search/Config/Schedulers/ScheluderConfig.java
+++ elastic-search/src/main/java/com/cluster/elastic_search/Config/Schedulers/ScheluderConfig.java
@@ -1,4 +1,4 @@
-package com.cluster.elastic_search.Config;
+package com.cluster.elastic_search.Config.Schedulers;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
